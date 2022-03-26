ec1204a8fb4bd677a0ed724b3401eb3a8ecbc50b
Vitor Junior <vitorjunior@poli.ufrj.br>
2022-03-26 12:27:17 -0300

    FilmPage

--- src/pages/FilmPage.tsx
+++ src/pages/FilmPage.tsx
@@ -1,4 +1,30 @@
+import { useParams } from "react-router-dom";
+import { useMovieDays } from "../adapters";
 
 export default () => {
-    return null;
+    
+    const {idFilme} = useParams();
+
+    if(idFilme) {
+        const [days] = useMovieDays(idFilme); 
+
+        return (
+            <main>
+                <h3>Selecione o horário</h3>
+                <ul>
+                    {days?days.days.map(({weekday, date, showtimes, id}) => 
+                        <li key={id}>
+                            <header>{weekday} - {date}</header>
+                            {showtimes?showtimes.map(({name}) => name):''}
+                        </li>
+                    ):''}
+                </ul>
+            </main>
+        );
+    } else {
+        return (
+            <h1>Página inválida!</h1>
+        );
+    }
+    
 }
