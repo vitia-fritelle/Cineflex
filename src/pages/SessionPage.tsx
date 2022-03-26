e45760c0402c0393cbeeb69769f53f29b557a01a
Vitor Junior <vitorjunior@poli.ufrj.br>
2022-03-26 13:04:06 -0300

    SessionPage

--- src/pages/SessionPage.tsx
+++ src/pages/SessionPage.tsx
@@ -1,4 +1,41 @@
+import { useParams } from "react-router-dom";
+import { useMovieSession } from "../adapters";
 
 export default () => {
-    return null;
+    
+    const {idSessao} = useParams();
+    
+    if(idSessao) {
+        const [session] = useMovieSession(idSessao);
+    
+        return (
+            <main>
+                <h3>Selecione o(s) assento(s)</h3>
+                <ul>
+                    {session?session.seats.map(({name, isAvailable, id}) => {
+                        return (
+                            <li key={id}>
+                                {name}
+                            </li>
+                        );
+                    }):''}
+                </ul>
+                <ul>
+                    <li>Selecionado</li>
+                    <li>Disponível</li>
+                    <li>Indisponível</li>
+                </ul>
+
+                <form action="/success" method="post">
+                    Nome do comprador:
+                    <input type="text" name="" id="" placeholder="Digite seu nome..."/>
+                    CPF do comprador:
+                    <input type="text" name="" id="" placeholder="Digite seu CPF..."/>
+                    <button>Reservar assento(s)</button>
+                </form>
+            </main>
+        );
+    } else {
+        return <h1>Página inválida</h1>
+    }
 }
