ae3f6ce6763e0f7313a9456e9492ee8d1f09379e
Vitor Junior <vitorjunior@poli.ufrj.br>
2022-03-26 01:49:31 -0300

    Adapters(API)

--- src/adapters/index.tsx
+++ src/adapters/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Movie, MovieDays, MovieSession } from "../types";
 
 const axios = require('axios');
@@ -6,32 +7,64 @@ const myAxios = axios.create({
     baseURL: 'https://mock-api.driven.com.br/api/v5/cineflex/'
 });
 
-export const getMoviesList = async () => {
-    
-    const {data: movies}: {data: Array<Movie>} = await myAxios.get('movies');
-    return movies;
+export const useMovies = (): [
+    Array<Movie>, (movies: Array<Movie>) => void
+] => {
+    const [movies, setMovies]: [
+        Array<Movie>, (movies: Array<Movie>) => void
+    ] = useState(Array(0));
+    useEffect(() => {
+        (async () => setMovies((await myAxios.get('movies')).data))();
+    })
+
+    return [movies, setMovies];
 };
 
-export const getMovieDays = async (movieId: number): Promise<MovieDays> => {
+export const useMovieDays = (movieId: number): [
+    MovieDays, (days: MovieDays) => void
+] => {
     
-    const {data: days}: {data: MovieDays} = myAxios.get(`movies/${movieId}/showtimes`);
-    return days;
+    const [days, setDays]: [
+        MovieDays, (days: MovieDays) => void
+    ] = useState(Object());
+    
+    useEffect(() => {
+        (async () => {
+            setDays((await myAxios.get(`movies/${movieId}/showtimes`)).data);
+        })();
+    });
+
+    return [days, setDays];
 }
 
-export const getMovieSession = async (sessionId: number): Promise<MovieSession> => {
+export const useMovieSession = (sessionId: number): [
+    MovieSession, (session: MovieSession) => void
+] => {
     
-    const {data: session}: {data: MovieSession} = myAxios.get(`showtimes/${sessionId}/seats`);
-    return session;
+    const [session, setSession]: [
+        MovieSession, (session: MovieSession) => void
+    ] = useState(Object());
+
+    useEffect(() => {
+        (async () => {
+            setSession((await myAxios.get(`showtimes/${sessionId}/seats`)).data);
+        })();
+    });
+    
+    return [session, setSession];
 }
 
 export const reserveSeats = async (
-    ids: Array<number>, name: string, cpf: string) => {
+    ids: Array<number>, name: string, cpf: string
+) => {
     
     const objeto = {
         ids,
         name,
         cpf
-    }
+    };
+
     const {data} = await axios.post('seats/book-many', objeto);
+
     return data
 }
